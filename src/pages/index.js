a2693bcf4666eb4c9faf2d153b654b52f64f526d
Devyn Lowry <devynwlowry@gmail.com>
2023-04-07 16:53:26 -0400

    Added NextUI

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,26 @@ export default function Home() {
       <div>
         <Navbar />
       </div>
-      <div className='grid grid-cols-2 gap-x-12 gap-y-16 content-evenly m-12'>
-        <div className='bg-transparent col-span-1 row-span-auto border-2 border-white rounded-xl p-4'>
-          <p>Hello! My name is Devyn 👋 I'm a Product Designer who has been coming up with cool ideas since high school.</p>
+      <div>
+      <div className='grid place-items-center grid-cols-3 grid-rows-9 gap-x-12 gap-y-16 m-12'>
+        <div className='bg-white col-span-2 border-2 border-gray rounded-xl p-4 drop-shadow-2xl'>
+          <p className='font-sans text-5xl font-bold w-auto text-black'>Hello! My name is Devyn 👋 I'm a Product Designer who has been coming up with cool ideas since high school.</p>
           <div>
             <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <div className='flex justify-between align-center gap-4'>
-              <button className='bg-transparent h-12 w-72 border-2 border-white rounded-full'>
+              <button className='bg-black h-12 w-1/2 border-2 border-white rounded-full text-xl text-white font-semibold'>
                 Let's Work!
                 <Link href="" />
               </button>
               <div className='contents'>
                 <div className='flex gap-4'>
-                    <button className='rounded-full bg-transparent h-auto w-auto border-2 border-white'>
+                    <button className='rounded-full bg-black h-auto w-auto border-2 border-white'>
                     <Image
                       src="/../public/images/2021 Twitter logo - white.png"
                       height={32}
@@ -29,7 +36,7 @@ export default function Home() {
                       className='h-4 w-5 m-3'
                     />
                   </button>
-                  <button className='rounded-full bg-transparent h-auto w-auto border-2 border-white'>
+                  <button className='rounded-full bg-black h-auto w-auto border-2 border-white'>
                     <Image
                       src="/../public/images/2021 Twitter logo - white.png"
                       height={32}
@@ -38,7 +45,7 @@ export default function Home() {
                       className='h-4 w-5 m-3'
                     />
                   </button>
-                  <button className='rounded-full bg-transparent h-auto w-auto border-2 border-white'>
+                  <button className='rounded-full bg-black h-auto w-auto border-2 border-white'>
                     <Image
                       src="/../public/images/2021 Twitter logo - white.png"
                       height={32}
@@ -47,7 +54,7 @@ export default function Home() {
                       className='h-4 w-5 m-3'
                     />
                   </button>
-                  <button className='rounded-full bg-transparent h-auto w-auto border-2 border-white'>
+                  <button className='rounded-full bg-black h-auto w-auto border-2 border-white'>
                     <Image
                       src="/../public/images/2021 Twitter logo - white.png"
                       height={32}
@@ -56,7 +63,7 @@ export default function Home() {
                       className='h-4 w-5 m-3'
                     />
                   </button>
-                  <button className='rounded-full bg-transparent h-auto w-auto border-2 border-white'>
+                  <button className='rounded-full bg-black h-auto w-auto border-2 border-white'>
                     <Image
                       src="/../public/images/2021 Twitter logo - white.png"
                       height={32}
@@ -70,10 +77,12 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='bg-transparent col-span-1 row-span-auto border-2 border-white rounded-xl p-4'>
-          <p>Hello! My name is Devyn 👋 I’m a Product Designer who has been coming up with cool ideas since high school.</p>
+        <div className='bg-white grid-cols-3 border-2 border-gray rounded-xl p-4 shadow-2xl'>
+          <h1 className='font-sans text-5xl font-bold w-auto text-black'>Skills</h1>
         </div>
       </div>
+      </div>
+      
     </>
   )
 }
