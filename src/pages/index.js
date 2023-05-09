cfd1695a3e17b80ccb4f0500a04a365eab4251ae
Devyn Lowry <devynwlowry@gmail.com>
2023-05-08 23:44:23 -0400

    Fixing background image, added svg files, added color animation to name

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,22 @@ export default function Home() {
   return (
     <>
     <div>
-      <Navbar />
+      <Navbar/>
     </div>
-    <div className='grid grid-cols-1 m-4 items-end overflow-hidden' id="grid container">
-      <div id="col1">
-          <h1 className='text-[16vh] text-white font-monumentblack uppercase absolute bottom-0'>
-            devyn 
-            <br/>
-            lowry
-          </h1>
+    <body className='bg-[#F4F4FC] dark:bg-[#111] bg-array bg-repeat dark:bg-arraywhite'>
+      <div className='grid grid-cols-1 m-4' id="grid container">
+        <div id="col1">
+            <h1 className='text-[16vh] animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-monumentblack uppercase 
+            absolute bottom-0 
+            sm:text-[8vh] md:text-[12vh] lg:text-[16vh] xl:text-[18vh] 2xl:text-[20vh]'>
+              devyn 
+              <br/>
+              lowry
+            </h1>
+        </div>
       </div>
-    </div>
+    </body>
+    
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
@@ -8,60 +9,23 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        scroll: 'scroll 40s linear infinite',
+        text: 'text 5s ease infinite',
       },
       keyframes: {
-        scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(calc(-250x * 14))' },
+        text: {
+          '0%': { 'background-size': '200% 200%', 'background-position': 'left center' },
+          '100%': { 'background-size': '200% 200%', 'background-position': 'left center' },
+          '50%': { 'background-size': '200% 200%', 'background-position': 'right center' },
         },
       },
-    //   backgroundImage: {
-    //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-    //     'gradient-conic':
-    //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-    //   },
-    //   animation: {
-    //     'gradient-x':'gradient-x 15s ease infinite',
-    //     'gradient-y':'gradient-y 15s ease infinite',
-    //     'gradient-xy':'gradient-xy 15s ease infinite',
-    //   },
-    //   keyframes: {
-    //     'gradient-y': {
-    //       '0%, 100%': {
-    //         'background-size':'400% 400%',
-    //         'background-position': 'center top'
-    //       },
-    //       '50%': {
-    //         'background-size':'200% 200%',
-    //         'background-position': 'center center'
-    //         }
-    //     },
-    //     'gradient-x': {
-    //         '0%, 100%': {
-    //           'background-size':'200% 200%',
-    //           'background-position': 'left center'
-    //         },
-    //         '50%': {
-    //           'background-size':'200% 200%',
-    //           'background-position': 'right center'
-    //         }
-    //     },
-    //     'gradient-xy': {
-    //         '0%, 100%': {
-    //           'background-size':'400% 400%',
-    //           'background-position': 'left center'
-    //         },
-    //         '50%': {
-    //           'background-size':'200% 200%',
-    //           'background-position': 'right center'
-    //         }
-    //     }
-    // }
       fontFamily: {
         'monumentblack': ['monument-black', 'sans-serif'],
         'montrealbold': ['montreal-bold', 'sans-serif'],
         'montrealmedium': ['montreal-medium', 'sans-serif'],
+      },
+      backgroundImage: {
+        'array': "url('../../public/images/mini.svg')",
+        'arraywhite': "url('../../public/images/mini_white.svg')",
       },
     },
   },
