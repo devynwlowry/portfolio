a75286d6e1b1551c31836980759b9f102334937c
Devyn Lowry <devynwlowry@gmail.com>
2023-06-13 12:59:38 -0400

    added links for design is a problem

--- src/components/problem.js
+++ src/components/problem.js
@@ -1,12 +1,14 @@
+import Link from "next/link"
+
 export const Problem = () => {
 
     return (
         <>
-        <div className="fixed h-auto bottom-0 right-3 transform rotate-90 origin-right m-auto">
+        <Link href='/problem 'className="fixed h-auto bottom-0 right-3 transform rotate-90 origin-right m-auto">
             <p className='font-montrealbold text-light-accent-color
             iphone6:text-[1.3vh] ipadpro12:text-[1.25vh]
             dark:text-dark-accent-color'>DESIGN IS A PROBLEM.</p>
-        </div>
+        </Link>
         </>
     )
 }
