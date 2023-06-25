a67356bba7b29aa85dba4d272a5c4efbe3306538
Devyn Lowry <devynwlowry@gmail.com>
2023-06-25 15:13:25 -0400

    removed path for design is a problem until that page is ready

--- src/components/problem.js
+++ src/components/problem.js
@@ -4,7 +4,7 @@ export const Problem = () => {
 
     return (
         <>
-        <Link href='/problem 'className="fixed h-auto bottom-0 right-3 transform rotate-90 origin-right m-auto">
+        <Link href='/'className="fixed h-auto bottom-0 right-3 transform rotate-90 origin-right m-auto">
             <p className='font-montrealbold text-light-accent-color
             iphone6:text-[1.3vh] ipadpro12:text-[1.25vh]
             dark:text-dark-accent-color'>DESIGN IS A PROBLEM.</p>
