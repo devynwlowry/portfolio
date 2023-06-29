ab8f5ecabcda93f8712d54330ad980b982a7beda
Devyn Lowry <devynwlowry@gmail.com>
2023-06-29 17:29:19 -0400

    added an under construction page, updated tag line again, updated breadcrumbs to account for design is a problem page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,9 +15,9 @@ export default function contact() {
                 <div className='flex flex-col gap-8 justify-center align-center m-auto' id='projects'>
                     <Link href="mailto:devyn@devynlowry.com" className='font-montrealbold underline text-light-primary-text dark:text-dark-primary-text text-center'>CLICK ME.</Link>
                     <div className='flex flex-row gap-4'>
-                        <a target="_blank" href="https://www.linkedin.com/in/devyn-lowry-489a3810a/" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>linkedin.</a>
-                        <a target="_blank" href="https://twitter.com/devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>twitter.</a>
-                        <a target="_blank" href="https://medium.com/@devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>medium.</a>
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/devyn-lowry-489a3810a/" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>linkedin.</a>
+                        <a target="_blank" rel="noreferrer" href="https://twitter.com/devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>twitter.</a>
+                        <a target="_blank" rel="noreferrer" href="https://medium.com/@devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>medium.</a>
                     </div>
                 </div>
             </section>
