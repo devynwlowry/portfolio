5968739fe6c25b2b1d5ab937ac629e802797533e
Devyn Lowry <devynwlowry@gmail.com>
2023-06-29 17:18:21 -0400

    added links to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,11 +12,14 @@ export default function contact() {
             <section className='h-screen bg-light-primary-background
             flex flex-col
             dark:bg-dark-primary-background'>
-                <div id='top' className=''>
+                <div className='flex flex-col gap-8 justify-center align-center m-auto' id='projects'>
+                    <Link href="mailto:devyn@devynlowry.com" className='font-montrealbold underline text-light-primary-text dark:text-dark-primary-text text-center'>CLICK ME.</Link>
+                    <div className='flex flex-row gap-4'>
+                        <a target="_blank" href="https://www.linkedin.com/in/devyn-lowry-489a3810a/" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>linkedin.</a>
+                        <a target="_blank" href="https://twitter.com/devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>twitter.</a>
+                        <a target="_blank" href="https://medium.com/@devynwlowry" className='font-montrealbold underline uppercase text-light-primary-text dark:text-dark-primary-text'>medium.</a>
+                    </div>
                     
-                </div>
-                <div className='flex flex-col justify-center align-center m-auto' id='projects'>
-                    <Link href="mailto:devynwlowry@gmail.com" className='font-montrealbold underline text-light-primary-text dark:text-dark-primary-text'>CLICK ME.</Link>
                 </div>
             </section>
             <Problem/>
