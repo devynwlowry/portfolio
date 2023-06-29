dbd2dd1af9bb18890045ef4a61ef3e43df1b73f0
Devyn Lowry <devynwlowry@gmail.com>
2023-06-29 17:38:24 -0400

    changed concept to case study for complex card

--- src/components/cards/complex.js
+++ src/components/cards/complex.js
@@ -22,7 +22,7 @@ export const Complex = () => {
                 alt='complex app'
                 className="hidden dark:block"
             />
-            <h4 className='font-montrealbold text-light-secondary-text dark:text-dark-secondary-text'>CONCEPT //</h4>
+            <h4 className='font-montrealbold text-light-secondary-text dark:text-dark-secondary-text'>CASE STUDY //</h4>
             <h3 className='font-machinainkultra text-light-primary-text uppercase 
             iphonese:text-xl iphone6:text-2xl iphoneplus:text-2xl iphonexrmax:text-2xl ipad:text-3xl ipadpro:text-2xl ipadpro12:text-3xl
             dark:text-dark-primary-text'>Complex networks</h3>
