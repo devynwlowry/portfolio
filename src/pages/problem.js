e0968608f2df8c764c7f50e4452a005a18934506
Devyn Lowry <devynwlowry@gmail.com>
2023-06-29 17:31:01 -0400

    added an under construction page, updated tag line again, updated breadcrumbs to account for design is a problem page

---
+++ src/pages/problem.js
@@ -0,0 +1,20 @@
+import { BackButton } from "@/components/backbutton";
+import { Layout } from "@/components/layout";
+
+export default function Problem() {
+    return (
+        <>
+        <Layout/>
+        <BackButton/>
+        <section className="h-screen flex bg-light-primary-background dark:bg-dark-primary-background">
+            <div className="justify-center align-center m-auto">
+                <h1 className="font-montrealbold uppercase
+                
+                ">
+                    This page is under construction.
+                </h1>
+            </div>
+        </section>
+        </>
+    )
+}
